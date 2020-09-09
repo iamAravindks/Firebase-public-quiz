cf8adbd7c842e8cff2ed701c515581bff898e36c
iamAravinks <aravindkadaikattu@gmail.com>
2020-09-09 14:53:37 +0530

    add login page and google auth

--- public/login.js
+++ public/login.js
@@ -1,3 +1,6 @@
+let formContainer = document.querySelector(".forms-container")
+let loginDiv = document.querySelector(".login")
+
 document.addEventListener("DOMContentLoaded", event => {
 
     app = firebase.app()
@@ -8,14 +11,14 @@ document.addEventListener("DOMContentLoaded", event => {
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             // User is signed in.
-            console.log(user)
-            document.querySelector(".forms-container").getElementsByClassName.display = "none"
-            document.querySelector(".login").getElementsByClassName.display = "block"
+            formContainer.style.display = "none"
+            loginDiv.style.display = "block"
+            document.querySelector("#welcome").innerHTML = `Welcome ${user.displayName}`
 
         } else {
             // No user is signed in.
-            document.querySelector(".forms-container").getElementsByClassName.display = "all"
-            document.querySelector(".login").getElementsByClassName.display = "none"
+            formContainer.style.display = "all"
+            loginDiv.style.display = "none"
         }
     });
 
@@ -30,17 +33,22 @@ const login = () => {
         var errorMessage = error.message;
         alert("Error :", errorMessage)
         // ...
+
     });
 
 }
 
-const signinGoogle = () => {
+function signinGoogle() {
     console.log("CALLED THIS FUNVTION")
+
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
         var token = result.credential.accessToken;
+        // The signed-in user info.
         var user = result.user;
-        console.log(user, "login sucessful")
+
+        // console.log("logged")
 
         // ...
     }).catch(function (error) {
@@ -48,11 +56,31 @@ const signinGoogle = () => {
         var errorCode = error.code;
         var errorMessage = error.message;
         // The email of the user's account used.
-        console.log(errorMessage)
         var email = error.email;
         // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
         // ...
-
     });
+}
+
+const showQUiz = () => {
+    let tableBody = document.getElementById("t-body")
+    loginDiv.style.display = "none"
+    let tBody = document.getElementById("table")
+    tBody.classList.add("display")
+    console.log("working")
+    db.collection("admin").get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+            console.log(doc.data().Name);
+            console.log(doc.data().Quiz.length)
+            let tableCont = `<tr>
+                    <td>${doc.data().Name}</td>
+                    <td>${doc.data().Quiz.length}</td>
+                      </tr>`
+
+            tableBody.insertAdjacentHTML("beforeend", tableCont);
+
+
+        });
+    })
 }
